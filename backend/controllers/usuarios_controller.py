834f11af099845a53b3d3eef7b394fc804cf00a3
Alan Muñoz <almunoz24@alumnos.utalca.cl>
2025-08-16 18:06:26 -0400

    Implementacion basica de guardado de usuarios con nombre, contraseña e id personal, unica seguridad es: no se puede ingresar un usuario si el nombre o contraseña ya existen en la base de datos interna (archivo json). tambien hay archivos que se generaron solos con la ejecución del servidor, no se que hacen o para que sirben pero ahí están. el flujoutilizado es: app.py -> routes -> controller -> services -> json

--- backend/controllers/usuarios_controller.py
+++ backend/controllers/usuarios_controller.py
@@ -1,4 +1,7 @@
-from services.usuarios_service import leer_usuarios #importar los servicios de usuarios
+from services.usuarios_service import leer_usuarios, guardar_usuarios #importar los servicios de usuarios
 
 def obtener_todos_los_usuarios(): #retornar todos los usuarios
     return leer_usuarios()
+
+def guardar_nuevos_usuarios(usuarios):
+    guardar_usuarios(usuarios)
--- backend/routes/usuarios_routes.py
+++ backend/routes/usuarios_routes.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
-from controllers.usuarios_controller import obtener_todos_los_usuarios
+from controllers.usuarios_controller import obtener_todos_los_usuarios, guardar_nuevos_usuarios
 
 router=APIRouter()
 
 @router.get("/usuarios")
 def ruta_obtener_usuarios(): #enrutador para obtener los usuarios
     return {"usuarios":obtener_todos_los_usuarios()}
+
+@router.post("/usuarios/registrar")
+def registrar_usuario(nombre, contrasena):
+    usuarios=obtener_todos_los_usuarios()
+    if any(usuario["nombre"]==nombre for usuario in usuarios) or any(usuario["contrasena"]==contrasena for usuario in usuarios):
+        return {"mensaje":"usuario ya registrado o contraseña ya utilizada"}
+    nuevo_usuario={"id":len(usuarios)+1, "nombre":nombre, "contrasena":contrasena}
+    usuarios.append(nuevo_usuario)
+    guardar_nuevos_usuarios(usuarios)
+    return {"mensaje":"usuario ingresado correctamente"}
