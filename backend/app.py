0729de9747cdd06f7868cbaf15350b9d7d56be4a
Alan Muñoz <167464537+alanmunozcabello@users.noreply.github.com>
2025-08-17 16:08:27 -0400

    Comando para iniciar servidor como publico
    con este comando se puede iniciar el servidor como publico para toda la red publica o privada (se supone), esto puede ser util para pruebas tipo servidor dedicado.

--- backend/app.py
+++ backend/app.py
@@ -5,3 +5,4 @@ app = FastAPI()
 app.include_router(usuarios_routes.router)
 
 #iniciar servidor: python -m uvicorn app:app --reload
+#iniciar servidor publico: python -m uvicorn app:app --host 0.0.0.0 --port 8000
