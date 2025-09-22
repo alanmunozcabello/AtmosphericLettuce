08e9d81bce85d390558e3c01ae307dd63f0cd317
Cristian <caliaga24@alumnos.utalca.cl>
2025-09-22 18:10:18 -0300

    Eliminacion del reset (John)

--- web/js/login.js
+++ web/js/login.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // Estado de carga OFF: restaura el botón
       btn.textContent = 'Iniciar Sesión';
       btn.classList.remove('loading');
-
+      form.reset()
       // Redirección si todo está correcto
       window.location.href = 'home.html';
     }, 1200);
