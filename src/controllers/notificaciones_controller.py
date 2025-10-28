ee567842665b2428b98c7763b4e7f026201a123c
Nicolas-Urbina <nurbina24@alumnos.utalca.cl>
2025-10-28 06:42:17 -0300

    limpieza de codigo, elimine los metodos y los archvios relacionados con los pdf por la complejdad y su enficinecia para solo quedarnos con planillas html

--- src/controllers/notificaciones_controller.py
+++ src/controllers/notificaciones_controller.py
@@ -1,11 +1,5 @@
 from services.notificaciones_service import enviar_archivo, verificar_conexion_gmail
 
-
-
-
-def controller_enviar_pdf(correo):
-    return enviar_archivo(correo,"services/Archivos_pdf/pdf_modificado.pdf") 
-
 def controller_enviar_html(correo):
     return enviar_archivo(correo,"services/Archivos_HTML/salida.html")
 
--- src/routes/notificaciones_routes.py
+++ src/routes/notificaciones_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from controllers.notificaciones_controller import controller_enviar_codigo, controller_enviar_html, controller_enviar_pdf, controller_verificar_gmail
-from controllers.modificadora_notificaciones import controller_modificar_html, controller_modificar_pdf
+from controllers.notificaciones_controller import controller_enviar_codigo, controller_enviar_html, controller_verificar_gmail
+from controllers.modificadora_notificaciones import controller_modificar_html
 
 router=APIRouter()
 
@@ -9,10 +9,6 @@ def ruta_verificar_gmail():
   """Endpoint para verificar que la conexión con Gmail API funciona"""
   return controller_verificar_gmail()
 
-@router.post("/notificaciones/enviar_pdf")
-def ruta_enviar_pdf(correo):
-  controller_modificar_pdf(correo)
-  return controller_enviar_pdf(correo)
 
 @router.post("/notificaciones/enviar_html")
 def ruta_enviar_html(correo):
