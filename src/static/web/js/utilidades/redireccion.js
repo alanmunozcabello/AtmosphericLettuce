cd33c6b8141a0956c263ac242564f67011cf0fbc
Cristian <caliaga24@alumnos.utalca.cl>
2025-11-09 18:15:52 -0300

    Linter en algunos js aplicado

--- src/static/web/js/utilidades/redireccion.js
+++ src/static/web/js/utilidades/redireccion.js
@@ -1,20 +1,20 @@
 // Tiempo inicial del contador
-let countdown = 3;
-const countdownElement = document.getElementById('countdown');
+let countdown = 3
+const countdownElement = document.getElementById('countdown')
 
 // Inicia el temporizador
 const timer = setInterval(() => {
-    countdown--;
-    countdownElement.textContent = countdown;
+  countdown--
+  countdownElement.textContent = countdown
 
-    if (countdown <= 0) {
-        clearInterval(timer);
-        window.location.href = 'index.html';
-    }
-}, 1000);
+  if (countdown <= 0) {
+    clearInterval(timer)
+    window.location.href = 'index.html'
+  }
+}, 1000)
 
 // También redirigir inmediatamente si se hace clic en cualquier lugar
 document.addEventListener('click', () => {
-    clearInterval(timer);
-    window.location.href = 'index.html';
-});
+  clearInterval(timer)
+  window.location.href = 'index.html'
+})
